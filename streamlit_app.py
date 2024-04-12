2d19af702fdaaf4f59315b27b1e3808467763039
EEErinnern <97386435+EEErinnern@users.noreply.github.com>
2024-04-12 15:58:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,13 +48,13 @@ with st.spinner(text="Loading section..."):
 from constant import career_achievements
 st.subheader('Career Achievements 🥇')
 for position, achievements in career_achievements.items():
+    st.subheader(f'As {position}')
     achievement_list = ''.join([f'<li>{achievement}</li>' for achievement in achievements])
     st.markdown(f'<ul>{achievement_list}</ul>', unsafe_allow_html=True)
 
 linkedin_badge_html = """
 <div class="badge-base LI-profile-badge" data-locale="zh_CN" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="yiping-chen-163002225" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://de.linkedin.com/in/yiping-chen-163002225?trk=profile-badge">Yiping Chen</a></div>
 <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>"""
-st.components.v1.html(linkedin_badge_html, height=400)
 st.sidebar.markdown(linkedin_badge_html, unsafe_allow_html=True)
 
 st.sidebar.caption('Wish to connect?')
