2564a0a9600493d237c99a03c9d314d2e2cb70df
EEErinnern <97386435+EEErinnern@users.noreply.github.com>
2024-04-12 15:31:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,13 +49,14 @@ st.subheader('Career Achievements 🥇')
 achievement_list = ''.join(['<li>'+item+'</li>' for item in info['achievements']])
 st.markdown('<ul>'+achievement_list+'</ul>',unsafe_allow_html=True)
 
+linkedin_badge_html = """
+<div class="badge-base LI-profile-badge" data-locale="zh_CN" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="yiping-chen-163002225" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://de.linkedin.com/in/yiping-chen-163002225?trk=profile-badge">Yiping Chen</a></div>
+<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>"""
+st.components.v1.html(linkedin_badge_html, height=400)
+st.sidebar.markdown(linkedin_badge_html, unsafe_allow_html=True)
+
 st.sidebar.caption('Wish to connect?')
 st.sidebar.write('📧: chenerin1995@gmail.com')
 
 pdfFileObj = open('photos/RESUME_YIPING_CHEN_copy.pdf', 'rb')
 st.sidebar.download_button('download resume',pdfFileObj,file_name='RESUME_YIPING_CHEN_copy.pdf',mime='pdf')
-
-linkedin_badge_html = """
-<div class="badge-base LI-profile-badge" data-locale="zh_CN" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="yiping-chen-163002225" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://de.linkedin.com/in/yiping-chen-163002225?trk=profile-badge">Yiping Chen</a></div>
-"""
-st.components.v1.html(linkedin_badge_html, height=400)
